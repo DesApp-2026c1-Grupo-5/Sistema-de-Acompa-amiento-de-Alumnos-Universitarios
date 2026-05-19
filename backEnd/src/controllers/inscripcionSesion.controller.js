78ef73e9fb59d57a0b566cded7910d39ced1eaa7
FernandoFolcher <ferfolcher@gmail.com>
2026-05-19 19:46:48 -0300

    Adaptar controllers de sesiones e inscripciones a next(error)

--- backEnd/src/controllers/inscripcionSesion.controller.js
+++ backEnd/src/controllers/inscripcionSesion.controller.js
@@ -1,51 +1,77 @@
 const inscripcionSesionService = require("../services/inscripcionSesion.service");
 
-const inscribirse = async (req, res) => {
-  const inscripcion = await inscripcionSesionService.inscribirse(req.params.id, req.user.sub);
+const inscribirse = async (req, res, next) => {
+  try {
+    const inscripcion = await inscripcionSesionService.inscribirse(req.params.id, req.user.sub);
 
-  return res.status(201).json({
-    ok: true,
-    data: inscripcion,
-  });
+    return res.status(201).json({
+      ok: true,
+      data: inscripcion,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const cancelarMiInscripcion = async (req, res) => {
-  const data = await inscripcionSesionService.cancelarMiInscripcion(req.params.id, req.user.sub);
-
-  return res.status(200).json({
-    ok: true,
-    data,
-  });
+const inscribirseLegacy = async (req, res, next) => {
+  try {
+    req.params.id = req.body.sesion_id;
+    return await inscribirse(req, res, next);
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const aprobarParticipante = async (req, res) => {
-  const data = await inscripcionSesionService.aprobarParticipante(
-    req.params.id,
-    req.params.inscripcionId,
-    req.user.sub
-  );
+const cancelarMiInscripcion = async (req, res, next) => {
+  try {
+    const data = await inscripcionSesionService.cancelarMiInscripcion(req.params.id, req.user.sub);
 
-  return res.status(200).json({
-    ok: true,
-    data,
-  });
+    return res.status(200).json({
+      ok: true,
+      data,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const rechazarParticipante = async (req, res) => {
-  const data = await inscripcionSesionService.rechazarParticipante(
-    req.params.id,
-    req.params.inscripcionId,
-    req.user.sub
-  );
+const aprobarParticipante = async (req, res, next) => {
+  try {
+    const data = await inscripcionSesionService.aprobarParticipante(
+      req.params.id,
+      req.params.inscripcionId,
+      req.user.sub
+    );
 
-  return res.status(200).json({
-    ok: true,
-    data,
-  });
+    return res.status(200).json({
+      ok: true,
+      data,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const rechazarParticipante = async (req, res, next) => {
+  try {
+    const data = await inscripcionSesionService.rechazarParticipante(
+      req.params.id,
+      req.params.inscripcionId,
+      req.user.sub
+    );
+
+    return res.status(200).json({
+      ok: true,
+      data,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = {
   inscribirse,
+  inscribirseLegacy,
   cancelarMiInscripcion,
   aprobarParticipante,
   rechazarParticipante,
--- backEnd/src/controllers/sesionEstudio.controller.js
+++ backEnd/src/controllers/sesionEstudio.controller.js
@@ -1,49 +1,69 @@
 const sesionEstudioService = require("../services/sesionEstudio.service");
 
-const crearSesion = async (req, res) => {
-  const sesion = await sesionEstudioService.crearSesion(req.body, req.user.sub);
+const crearSesion = async (req, res, next) => {
+  try {
+    const sesion = await sesionEstudioService.crearSesion(req.body, req.user.sub);
 
-  return res.status(201).json({
-    ok: true,
-    data: sesion,
-  });
+    return res.status(201).json({
+      ok: true,
+      data: sesion,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const listarSesiones = async (req, res) => {
-  const result = await sesionEstudioService.listarSesiones(req.query, req.user.sub);
+const listarSesiones = async (req, res, next) => {
+  try {
+    const result = await sesionEstudioService.listarSesiones(req.query, req.user.sub);
 
-  return res.status(200).json({
-    ok: true,
-    data: result.data,
-    pagination: result.pagination,
-  });
+    return res.status(200).json({
+      ok: true,
+      data: result.data,
+      pagination: result.pagination,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const obtenerSesion = async (req, res) => {
-  const data = await sesionEstudioService.obtenerSesionPorId(req.params.id, req.user.sub);
+const obtenerSesion = async (req, res, next) => {
+  try {
+    const data = await sesionEstudioService.obtenerSesionPorId(req.params.id, req.user.sub);
 
-  return res.status(200).json({
-    ok: true,
-    data,
-  });
+    return res.status(200).json({
+      ok: true,
+      data,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const editarSesion = async (req, res) => {
-  const data = await sesionEstudioService.editarSesion(req.params.id, req.body, req.user.sub);
+const editarSesion = async (req, res, next) => {
+  try {
+    const data = await sesionEstudioService.editarSesion(req.params.id, req.body, req.user.sub);
 
-  return res.status(200).json({
-    ok: true,
-    data,
-  });
+    return res.status(200).json({
+      ok: true,
+      data,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
-const cancelarSesion = async (req, res) => {
-  const data = await sesionEstudioService.cancelarSesion(req.params.id, req.user.sub);
+const cancelarSesion = async (req, res, next) => {
+  try {
+    const data = await sesionEstudioService.cancelarSesion(req.params.id, req.user.sub);
 
-  return res.status(200).json({
-    ok: true,
-    data,
-  });
+    return res.status(200).json({
+      ok: true,
+      data,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = {
