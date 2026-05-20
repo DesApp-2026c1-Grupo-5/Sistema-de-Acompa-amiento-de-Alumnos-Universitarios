834a5a021da993a795a87258f2cae488df79454a
ArielValenzuela1 <arielsebastian.valenzuela@estudiantes.unahur.edu.ar>
2026-05-19 21:16:46 -0300

    feat(profile): agregar endpoints de privacidad y avatar

--- backEnd/src/routes/profile.routes.js
+++ backEnd/src/routes/profile.routes.js
@@ -7,5 +7,15 @@ const router = express.Router();
 
 router.get("/profile/me", authMiddleware, profileController.obtenerMiPerfil);
 router.put("/profile/me", authMiddleware, profileController.actualizarMiPerfil);
+router.patch(
+  "/profile/me/privacy",
+  authMiddleware,
+  profileController.actualizarPrivacidadMiPerfil
+);
+router.patch(
+  "/profile/me/avatar",
+  authMiddleware,
+  profileController.actualizarAvatarMiPerfil
+);
 
 module.exports = router;
