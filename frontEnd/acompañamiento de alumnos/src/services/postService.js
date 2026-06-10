a19e7c5d565b3852614a3ef21f33227ef5b6c1a6
FacundoNicolasTorres <facujaritor@gmail.com>
2026-06-10 15:20:01 -0300

    Paginación en feed estudiante y scroll infinito

--- frontEnd/acompañamiento de alumnos/src/services/postService.js
+++ frontEnd/acompañamiento de alumnos/src/services/postService.js
@@ -1,6 +1,7 @@
 import { api } from "./api";
 
-export const getPosts = () => api.get("/posts");
+export const getPosts = ({ page = 1, limit = 10 } = {}) =>
+  api.get(`/posts?page=${page}&limit=${limit}`);
 
 export const createPost = (contenido) => api.post("/posts", { contenido });
 
